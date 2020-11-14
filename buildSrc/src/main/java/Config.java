24ce6d20736e693f4d958762ba2acee6f9219106
Tsivilyova Nata <tsivilyova@magora-systems.com>
2020-11-14 13:29:03 +0700

    Initial commit

---
+++ buildSrc/src/main/java/Config.java
@@ -0,0 +1,8 @@
+public class Config {
+   public static final int compileSdkVersion = 28;
+   public static final String buildToolsVersion = "28.0.3";
+   public static final int minSdkVersion = 21;
+   public static final int targetVersion = 28;
+   public static final int versionCode = 1;
+   public static final String versionName = "1.0";
+}
